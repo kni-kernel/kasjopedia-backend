3aa4d7ae92644d4a45ab739d1fd2a05c03c6ad3e
Konrad Perłowski <44261671+Silnari@users.noreply.github.com>
2020-04-18 13:18:05 +0200

    [KASJ-27] Add REST endpoint to return all subject data (#2)
    * [KASJ-27] Add REST endpoint to return all subject data

---
+++ src/route/ModuleRoute.kt
@@ -0,0 +1,33 @@
+package io.project.route
+
+import io.ktor.application.call
+import io.ktor.http.HttpStatusCode
+import io.ktor.response.respond
+import io.ktor.routing.Route
+import io.ktor.routing.get
+import io.ktor.routing.route
+import io.project.model.Module
+import io.project.service.ModuleService
+
+val moduleService = ModuleService()
+
+fun Route.moduleRoute() {
+
+    route("/module") {
+        get("/{name}") {
+            call.respond(HttpStatusCode.OK, moduleService.getByName(call.parameters["name"]!!) ?: "")
+        }
+
+        get {
+            call.respond(HttpStatusCode.OK, moduleService.getAll())
+        }
+
+        get("/{fieldOfStudy}/{semester}") {
+            call.respond(
+                HttpStatusCode.OK, moduleService.getByFieldOfStudyAndSemester(
+                    call.parameters["fieldOfStudy"]!!, call.parameters["semester"]!!
+                )
+            )
+        }
+    }
+}
---
+++ src/service/ModuleService.kt
@@ -0,0 +1,47 @@
+package io.project.service
+
+import io.project.model.Module
+
+class ModuleService {
+
+    //mocked data base
+    private val db = mutableListOf<Module>()
+
+    init {
+        //test values
+        val vpython = Module("vpython", 3, "", 0)
+        vpython.setHours(15, 0, 30, 0, 0, 0)
+        val python = Module("python", 4, "", 0)
+        python.setHours(0, 0, 45, 0, 0, 0)
+        val cpp = Module("podstawy-programowania-obiektowego", 6, "is", 3)
+        cpp.setHours(30, 0, 30, 0, 0, 0)
+        val cpp2 = Module("programowanie-obiektowe-1", 6, "is", 4)
+        cpp2.setHours(30, 0, 30, 0, 0, 0)
+        val cpp3 = Module("programowanie-obiektowe-2", 6, "is", 5)
+        cpp3.setHours(30, 0, 30, 0, 0, 0)
+        val graphs = Module("teoria-grafow", 4, "is", 5)
+        graphs.setHours(30, 0, 30, 0, 0, 0)
+        db.add(python)
+        db.add(vpython)
+        db.add(cpp)
+        db.add(cpp2)
+        db.add(cpp3)
+        db.add(graphs)
+    }
+
+    fun getByName(name: String): Module? {
+        return db.find { module -> module.name == name }
+    }
+
+    fun getAll(): MutableList<Module> {
+        return db
+    }
+
+    fun getByFieldOfStudyAndSemester(fieldOfStudy: String, semester: String): List<Module> {
+        return try {
+            db.filter { module -> module.fieldOfStudy == fieldOfStudy && module.semester == semester.toInt() }
+        } catch (e: NumberFormatException) {
+            emptyList()
+        }
+    }
+}
